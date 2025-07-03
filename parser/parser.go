89d581b2b2870d5836e453ccd92c51945744d1d8
UnnamedXAer <UnnamedXAer@gmail.com>
2025-07-03 14:49:44 +0200

    ast + parsing

---
+++ parser/parser.go
@@ -0,0 +1,52 @@
+package parser
+
+import (
+	"github.com/unnamedxaer/interpreter-in-go/ast"
+	"github.com/unnamedxaer/interpreter-in-go/lexer"
+	"github.com/unnamedxaer/interpreter-in-go/token"
+)
+
+type Parser struct {
+	l         *lexer.Lexer
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	//  Read two tokens, so curToken and peekToken are both set
+
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+
+	program := &ast.Program{}
+
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	return nil
+}
