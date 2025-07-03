19180360d53f8ab1da4771431d34714ff1c6bf59
UnnamedXAer <UnnamedXAer@gmail.com>
2025-07-03 11:31:10 +0200

    repl

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/unnamedxaer/interpreter-in-go/repl"
+)
+
 func main() {
 	print("interpreter in go\n")
+
+	user, err := user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Printf("Fell free to type commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
