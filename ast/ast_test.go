f25951656c66fc3e07f153665cc854b710a53a4f
UnnamedXAer <unnamedxaer.git@gmail.com>
2025-07-04 20:09:47 +0200

    finish test

--- ast/ast_test.go
+++ ast/ast_test.go
@@ -6,18 +6,26 @@ import (
 	"github.com/unnamedxaer/interpreter-in-go/token"
 )
 
-func TestString(t testing.T) {
+func TestString(t *testing.T) {
 
 	// let myVar = anotherVar;
 
 	program := &Program{
 		Statements: []Statement{
 			&LetStatement{
-				Token: token.Token{Type: token.LET},
+				Token: token.Token{Type: token.LET, Literal: "let"},
 				Name: &Idendifier{
-					Token:   token.IDENT,
-					Literal: "let",
-				}},
+					Token: token.Token{Type: token.IDENT, Literal: "myVar"},
+					Value: "myVar",
+				},
+				Value: &Idendifier{
+					Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
+					Value: "anotherVar"},
+			},
 		},
+	}
+
+	if program.String() != "let myVar = anotherVar;" {
+		t.Errorf("program.String() wrong. got=%q", program.String())
 	}
 }
