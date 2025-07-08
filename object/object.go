1ad198bbcf5d9c57ee2d7e829a8243eb18d356ed
UnnamedXAer <UnnamedXAer@gmail.com>
2025-07-08 11:59:31 +0200

    representation of int, bool, null types

---
+++ object/object.go
@@ -0,0 +1,37 @@
+package object
+
+import "fmt"
+
+const (
+	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
+)
+
+type ObjectType string
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Type() string    { return INTEGER_OBJ }
+func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
+
+type Boolean struct {
+	Value bool
+}
+
+func (i *Boolean) Type() string    { return BOOLEAN_OBJ }
+func (i *Boolean) Inspect() string { return fmt.Sprintf("%t", i.Value) }
+
+type Null struct {
+	Value bool
+}
+
+func (i *Null) Type() string    { return NULL_OBJ }
+func (i *Null) Inspect() string { return "null" }
